86a1d9130d568b8f284b54e20b690abea7913e15
gurhank <gurhankocyigit@gmail.com>
2020-06-05 15:34:33 -0500

    P1_EtsyTitleVerification.java

--- src/test/java/test/day2_findElement_getText_getAttribute/P1_EtsyTitleVerification.java
+++ src/test/java/test/day2_findElement_getText_getAttribute/P1_EtsyTitleVerification.java
@@ -2,7 +2,6 @@ package test.day2_findElement_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -20,7 +19,12 @@ public class P1_EtsyTitleVerification {
 
         // 3. Search for “wooden spoon”
 
-        driver.findElement(By.id("global-enhancements-search-query")).sendKeys("wooden spoon" + Keys.ENTER);
+        //driver.findElement(By.id("global-enhancements-search-query")).sendKeys("wooden spoon" + Keys.ENTER);
+        driver.findElement(By.id("global-enhancements-search-query")).sendKeys("wooden spoon");
+
+        //locating and clicking to the search button using class attribute value
+        //locating the search button
+        driver.findElement(By.className("wt-input-btn-group__btn")).click();
 
         // 4. Verify title:
         //Expected: “Wooden spoon | Etsy”
