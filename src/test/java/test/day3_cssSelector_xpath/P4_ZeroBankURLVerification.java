04c912edf7c5a8f2ab4ad1c3ef1c0f76b549b75f
gurhank <gurhankocyigit@gmail.com>
2020-06-09 13:16:03 -0500

    WebDriverFactory

--- src/test/java/test/day3_cssSelector_xpath/P4_ZeroBankURLVerification.java
+++ src/test/java/test/day3_cssSelector_xpath/P4_ZeroBankURLVerification.java
@@ -1,6 +1,5 @@
 package test.day3_cssSelector_xpath;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,7 +10,7 @@ public class P4_ZeroBankURLVerification {
 
       //TC #4: Zero Bank URL verification
         //1. Open Chrome browser
-        WebDriverManager.chromedriver().setup();
+        //WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
         //2. Go to http://zero.webappsecurity.com/login.html
