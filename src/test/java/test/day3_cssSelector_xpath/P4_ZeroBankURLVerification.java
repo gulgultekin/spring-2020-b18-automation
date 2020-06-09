f82f06fd464721c5a10e6a533b165ef28e963394
gurhank <gurhankocyigit@gmail.com>
2020-06-09 11:32:45 -0500

    warm up task 4, getAttributeValue

---
+++ src/test/java/test/day3_cssSelector_xpath/P4_ZeroBankURLVerification.java
@@ -0,0 +1,39 @@
+package test.day3_cssSelector_xpath;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class P4_ZeroBankURLVerification {
+    public static void main(String[] args) {
+
+      //TC #4: Zero Bank URL verification
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        //2. Go to http://zero.webappsecurity.com/login.html
+        driver.get("http://zero.webappsecurity.com/login.html");
+
+        //3. Get attribute value of href from the “forgot your password” link
+        //driver.findElement(By.linkText("Forgot your password ?"));
+        WebElement forgotPasswordLink = driver.findElement(By.partialLinkText("Forgot"));
+        //4. Verify attribute value contains
+        //Expected: “/forgot-password.html”
+
+        String actualHrefValue = forgotPasswordLink.getAttribute("href");
+        String expectedInHrefValue = "/forgot-password.html";
+
+        System.out.println("actualHrefValue = " + actualHrefValue);
+
+        if (actualHrefValue.contains(expectedInHrefValue)){
+            System.out.println("Href value verification PASSED!");
+        }else {
+            System.out.println("Href value verification FAILED!");
+        }
+
+        System.out.println("Is forgot password displayed? " +forgotPasswordLink.isDisplayed());
+    }
+}
