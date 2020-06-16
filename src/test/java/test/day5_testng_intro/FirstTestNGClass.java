cb896aa6ab492082475822bc7e15021c6e3ab838
gurhank <gurhankocyigit@gmail.com>
2020-06-16 15:35:27 -0500

    testNG intro

---
+++ src/test/java/test/day5_testng_intro/FirstTestNGClass.java
@@ -0,0 +1,18 @@
+package test.day5_testng_intro;
+
+import org.testng.annotations.Test;
+
+public class FirstTestNGClass {
+
+
+    @Test
+    public void test1(){
+        System.out.println("Test1 is running...");
+    }
+
+    public void test2(){
+        System.out.println("Test2 is running...");
+    }
+
+
+}
