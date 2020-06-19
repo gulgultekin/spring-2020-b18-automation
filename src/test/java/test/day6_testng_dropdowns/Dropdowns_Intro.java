c411745e1504813c6458a9c2997e2e70011c4b0e
gurhank <gurhankocyigit@gmail.com>
2020-06-19 15:11:53 -0500

    dropdowns examples

--- src/test/java/test/day6_testng_dropdowns/Dropdowns_Intro.java
+++ src/test/java/test/day6_testng_dropdowns/Dropdowns_Intro.java
@@ -1,6 +1,9 @@
 package test.day6_testng_dropdowns;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -38,6 +41,13 @@ public class Dropdowns_Intro {
 
         //3. Verify “Simple dropdown” default selected value is correct
         //Expected: “Please select an option”
+        Select simpleDropdown = new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
+
+        String actualDefaultOfSimpleDropdown = simpleDropdown.getFirstSelectedOption().getText();
+        String expectedDefaultOfSimpleDropdown = "Please select an option";
+
+        Assert.assertEquals(actualDefaultOfSimpleDropdown, expectedDefaultOfSimpleDropdown);
+
         //4. Verify “State selection” default selected value is correct
         //Expected: “Select a State”
 
