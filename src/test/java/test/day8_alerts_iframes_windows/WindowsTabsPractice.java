30b4e140d8487d47beed112d558cc9c6a787208c
gurhank <gurhankocyigit@gmail.com>
2020-06-26 15:31:03 -0500

    day8

--- src/test/java/test/day8_alerts_iframes_windows/WindowsTabsPractice.java
+++ src/test/java/test/day8_alerts_iframes_windows/WindowsTabsPractice.java
@@ -2,6 +2,7 @@ package test.day8_alerts_iframes_windows;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import utilities.WebDriverFactory;
 
 import java.util.concurrent.TimeUnit;
@@ -17,6 +18,10 @@ public class WindowsTabsPractice {
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+    @Test
+    public void p5_window_handle_practice(){
 
     }
 
