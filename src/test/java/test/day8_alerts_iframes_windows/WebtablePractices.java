7a66004f647f9e8f5c9ad85cf0e94ec06ca76c87
gurhank <gurhankocyigit@gmail.com>
2020-06-26 09:30:18 -0500

    day8

---
+++ src/test/java/test/day8_alerts_iframes_windows/WebtablePractices.java
@@ -0,0 +1,29 @@
+package test.day8_alerts_iframes_windows;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import utilities.WebDriverFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebtablePractices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        //TC #1: Smartbear software link verification
+        //1. Open browser
+        //2. Go to website:
+        //http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    
+
+}
