5d88ed933370fce094d63caeb43cb7e65cd239b5
gurhank <gurhankocyigit@gmail.com>
2020-06-26 10:40:23 -0500

    day 9

--- src/test/java/test/day8_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/test/day8_alerts_iframes_windows/DropdownPractices.java
@@ -1,7 +1,8 @@
 package test.day8_alerts_iframes_windows;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import utilities.WebDriverFactory;
 
 import java.util.concurrent.TimeUnit;
@@ -10,14 +11,20 @@ public class DropdownPractices {
 
     WebDriver driver;
 
-    @BeforeMethod
+    @BeforeClass
     public void setUp(){
+
 
         driver = WebDriverFactory.getDriver("chrome");
         driver.get("http://practice.cybertekschool.com/dropdown");
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void p7_month_dropdown_default_value_test(){
 
     }
 
