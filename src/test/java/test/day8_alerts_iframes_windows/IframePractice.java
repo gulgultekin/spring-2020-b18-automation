f16e290c0c95ec4a3e830df8b37b7cc04f4211db
gurhank <gurhankocyigit@gmail.com>
2020-06-26 14:50:34 -0500

    day8

--- src/test/java/test/day8_alerts_iframes_windows/IframePractice.java
+++ src/test/java/test/day8_alerts_iframes_windows/IframePractice.java
@@ -3,6 +3,7 @@ package test.day8_alerts_iframes_windows;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utilities.WebDriverFactory;
@@ -28,15 +29,24 @@ public class IframePractice {
 
         //1-Locating iframe as web element, then switching to it.
         WebElement iframe = driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']"));
+        //WebElement iframe2 = driver.findElement(By.tagName("iframe"));
 
         //switching to iframe using webElement
         driver.switchTo().frame(iframe);
 
         //4. Assert: “Your content goes here.” Text is displayed.
         WebElement yourContentGoesHereText = driver.findElement(By.xpath("//p"));
-        
-        //5. Assert: “An iFrame containing the TinyMCE WYSIWYG Editor
 
+        Assert.assertTrue(yourContentGoesHereText.isDisplayed());
+
+        //WE HAVE TO SWITCH BACK TO DEFAULT HTML TO BE ABLE TO LOCATE ANYTHING IN THERE
+        driver.switchTo().parentFrame();
+
+
+        //5. Assert: “An iFrame containing the TinyMCE WYSIWYG Editor
+        WebElement h3Text = driver.findElement(By.xpath("//h3"));
+
+        Assert.assertTrue(h3Text.isDisplayed());
 
     }
 
