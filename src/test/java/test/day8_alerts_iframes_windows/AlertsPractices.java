364dbd0c67155c396e13158766f9fe7968a0e88f
gurhank <gurhankocyigit@gmail.com>
2020-06-26 13:30:39 -0500

    day8

--- src/test/java/test/day8_alerts_iframes_windows/AlertsPractices.java
+++ src/test/java/test/day8_alerts_iframes_windows/AlertsPractices.java
@@ -1,7 +1,11 @@
 package test.day8_alerts_iframes_windows;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import utilities.WebDriverFactory;
 
 import java.util.concurrent.TimeUnit;
@@ -17,6 +21,26 @@ public class AlertsPractices {
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void p1_information_alert_test(){
+        //Locating the alert button to be able to click.
+        WebElement infoAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
+
+        //Clicking to the alert button so that the alert should pop-up
+        infoAlertButton.click();
+
+        //1- Create the Alert instance and switch to it
+        Alert alert = driver.switchTo().alert();
+
+        //2- Use "alert" to accept the alert.
+        alert.accept();
+
+        
+
+
 
     }
 
