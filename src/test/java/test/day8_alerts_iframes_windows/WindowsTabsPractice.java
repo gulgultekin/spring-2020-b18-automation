2cef721a9f20f10ff9eb1176019ba1c6d56c9c3a
gurhank <gurhankocyigit@gmail.com>
2020-06-26 15:30:06 -0500

    day8

---
+++ src/test/java/test/day8_alerts_iframes_windows/WindowsTabsPractice.java
@@ -0,0 +1,23 @@
+package test.day8_alerts_iframes_windows;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import utilities.WebDriverFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class WindowsTabsPractice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/iframe");
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+}
