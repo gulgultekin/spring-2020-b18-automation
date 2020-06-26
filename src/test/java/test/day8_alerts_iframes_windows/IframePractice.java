2913a7fd89de12e61432de53bfcdb873916d2094
gurhank <gurhankocyigit@gmail.com>
2020-06-26 14:34:55 -0500

    day8

--- src/test/java/test/day8_alerts_iframes_windows/IframePractice.java
+++ src/test/java/test/day8_alerts_iframes_windows/IframePractice.java
@@ -1,7 +1,10 @@
 package test.day8_alerts_iframes_windows;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import utilities.WebDriverFactory;
 
 import java.util.concurrent.TimeUnit;
@@ -17,6 +20,22 @@ public class IframePractice {
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void iframe_practice_test(){
+
+        //1-Locating iframe as web element, then switching to it.
+        WebElement iframe = driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']"));
+
+
+        //4. Assert: “Your content goes here.” Text is displayed.
+        WebElement yourContentGoesHereText = driver.findElement(By.xpath("//p"));
+
+
+        //5. Assert: “An iFrame containing the TinyMCE WYSIWYG Editor
+
 
     }
 
