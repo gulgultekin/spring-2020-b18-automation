c7367ed7e1d931d66110f26ca03521092982cebd
gurhank <gurhankocyigit@gmail.com>
2020-06-26 10:48:03 -0500

    day 9

--- src/test/java/test/day8_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/test/day8_alerts_iframes_windows/DropdownPractices.java
@@ -1,10 +1,14 @@
 package test.day8_alerts_iframes_windows;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import utilities.WebDriverFactory;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 public class DropdownPractices {
@@ -25,7 +29,22 @@ public class DropdownPractices {
 
     @Test
     public void p7_month_dropdown_default_value_test(){
+        // Locating the dropdown
+        Select monthDropdown = new Select(driver.findElement(By.xpath("//select[@id='month']")));
 
+        //We need to create our actual and expected values.
+        String actualDropdown = monthDropdown.getFirstSelectedOption().getText();
+
+        System.out.println("actualDropdown = " + actualDropdown);
+
+        String expectedDropdown = LocalDateTime.now().getMonth().name();
+
+        System.out.println("expectedDropdown = " + expectedDropdown);
+
+        //1- First way of doing this assertion
+        Assert.assertEquals(actualDropdown.toLowerCase(), expectedDropdown.toLowerCase());
+
+        
     }
 
 }
