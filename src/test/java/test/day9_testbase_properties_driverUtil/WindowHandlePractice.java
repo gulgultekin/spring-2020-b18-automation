1bfabcbecf5d0292f0ed9707c67063ed3556cfd4
gurhank <gurhankocyigit@gmail.com>
2020-06-30 11:40:07 -0500

    day 9

--- src/test/java/test/day9_testbase_properties_driverUtil/WindowHandlePractice.java
+++ src/test/java/test/day9_testbase_properties_driverUtil/WindowHandlePractice.java
@@ -1,30 +1,14 @@
 package test.day9_testbase_properties_driverUtil;
 
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import utilities.WebDriverFactory;
+import test.base.TestBase;
 
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
-public class WindowHandlePractice {
-
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setUpMethod(){
-
-        driver = WebDriverFactory.getDriver("chrome");
-
-        driver.manage().window().maximize();
-
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-    }
-
+public class WindowHandlePractice extends TestBase {
+    
     @Test
     public void multiple_window_test(){
 
