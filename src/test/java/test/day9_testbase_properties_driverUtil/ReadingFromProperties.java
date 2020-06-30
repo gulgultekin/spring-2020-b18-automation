8159f97528ed50a6c5a1a062484947d57ab7da34
gurhank <gurhankocyigit@gmail.com>
2020-06-30 13:41:13 -0500

    day 9

---
+++ src/test/java/test/day9_testbase_properties_driverUtil/ReadingFromProperties.java
@@ -0,0 +1,12 @@
+package test.day9_testbase_properties_driverUtil;
+
+import org.testng.annotations.Test;
+
+public class ReadingFromProperties {
+
+    @Test
+    public void reading_properties_file(){
+
+    }
+
+}
