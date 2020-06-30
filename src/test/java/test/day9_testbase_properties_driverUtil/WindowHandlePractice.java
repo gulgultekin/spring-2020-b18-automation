96e165baace4ded2d52caef48253d444176df829
gurhank <gurhankocyigit@gmail.com>
2020-06-30 10:39:12 -0500

    day 9

--- src/test/java/test/day9_testbase_properties_driverUtil/WindowHandlePractice.java
+++ src/test/java/test/day9_testbase_properties_driverUtil/WindowHandlePractice.java
@@ -6,6 +6,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utilities.WebDriverFactory;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class WindowHandlePractice {
@@ -34,7 +35,13 @@ public class WindowHandlePractice {
         ((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
 
         //5. Create a logic to switch to the tab where Etsy.com is open
-        
+        Set<String> windowHandles = driver.getWindowHandles();
+
+        for (String each : windowHandles) {
+
+            driver.switchTo().window(each);
+
+        }
         // 6. Assert: Title contains “Etsy”
 
 
