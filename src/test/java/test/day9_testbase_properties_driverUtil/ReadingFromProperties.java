b3ad890354c372b97b77c14118ed908453a6d4d9
gurhank <gurhankocyigit@gmail.com>
2020-06-30 13:47:39 -0500

    day 9

--- src/test/java/test/day9_testbase_properties_driverUtil/ReadingFromProperties.java
+++ src/test/java/test/day9_testbase_properties_driverUtil/ReadingFromProperties.java
@@ -2,10 +2,20 @@ package test.day9_testbase_properties_driverUtil;
 
 import org.testng.annotations.Test;
 
+import java.util.Properties;
+
 public class ReadingFromProperties {
 
     @Test
     public void reading_properties_file(){
+        //Properties class object allows us
+        // to read from configuration.properties
+        Properties properties = new Properties();
+
+        //We need to show where the file is stored.
+        //We need the path of the file
+        String path = "configuration.properties";
+        ///Users/cybertekchicago-1/Desktop/seleniumprojectb18/configuration.properties
 
     }
 
