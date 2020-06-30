d552eb5374807685378c785d586a5c0b1abae52f
gurhank <gurhankocyigit@gmail.com>
2020-06-30 14:38:12 -0500

    day 9

--- src/test/java/utilities/ConfigurationReader.java
+++ src/test/java/utilities/ConfigurationReader.java
@@ -1,12 +1,34 @@
 package utilities;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigurationReader {
 
     //1- we created properties object
+    private static Properties properties = new Properties();
+
+
+    static {
+        //2- get the path and store in String, or directly pass into fileInputStream obj
+        String path = "configuration.properties";
+
+        try {
+            //3- we need to open the file
+            FileInputStream file = new FileInputStream(path);
+            //4- we need to load the file to properties object
+            properties.load(file);
+
+            //5- close.file
+            file.close();
+
+        } catch (IOException e) {
+            System.out.println("Properties file not found.");
+        }
+    }
+
     
-    //2- get the path and store in String, or directly pass into fileInputStream obj
-    //3- we need to open the file
-    //4- we need to load the file
-    //5- close.file
+
 
 }
