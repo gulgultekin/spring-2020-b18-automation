d2822bea631d2ddddb3c5c2098f265144b912ddc
gurhank <gurhankocyigit@gmail.com>
2020-06-30 10:35:43 -0500

    day 9

---
+++ src/test/java/test/day9_testbase_properties_driverUtil/WindowHandlePractice.java
@@ -0,0 +1,45 @@
+package test.day9_testbase_properties_driverUtil;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.WebDriverFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class WindowHandlePractice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+
+        driver = WebDriverFactory.getDriver("chrome");
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void multiple_window_test(){
+
+        driver.get("https://www.amazon.com");
+
+        // we are downcasting our driver to JavascriptExecutor to be able to use its methods.
+        ((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+
+        //5. Create a logic to switch to the tab where Etsy.com is open
+        
+        // 6. Assert: Title contains “Etsy”
+
+
+
+
+    }
+
+}
