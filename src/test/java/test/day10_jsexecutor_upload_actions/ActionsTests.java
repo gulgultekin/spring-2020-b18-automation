22155835549f5a29e2d137c86aa8f9be646d1bb6
gurhank <gurhankocyigit@gmail.com>
2020-07-02 11:26:30 -0500

    day 10

---
+++ src/test/java/test/day10_jsexecutor_upload_actions/ActionsTests.java
@@ -0,0 +1,12 @@
+package test.day10_jsexecutor_upload_actions;
+
+import org.testng.annotations.Test;
+
+public class ActionsTests {
+
+    @Test
+    public void hover_over_test1(){
+        
+    }
+
+}
