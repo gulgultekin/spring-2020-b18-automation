c5b0402799008c2fc0f6391f1ef0ab641c1c0173
gurhank <gurhankocyigit@gmail.com>
2020-07-02 09:28:37 -0500

    day 10

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,7 +3,9 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class Driver {
 
@@ -26,11 +28,28 @@ public class Driver {
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
+                    break;
+                case "chrome-headless":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+                case "firefox-headless":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
+                    break;
+
             }
         }
 
         return driver;
 
+    }
+
+    public static void closeDriver(){
+        if (driver != null){
+            driver.quit();
+            driver=null;
+        }
     }
 
 
