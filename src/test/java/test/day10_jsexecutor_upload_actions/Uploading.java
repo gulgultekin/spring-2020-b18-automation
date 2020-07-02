a2d8b047b6a1f0cb8a374ba2920a8562e55eabd0
gurhank <gurhankocyigit@gmail.com>
2020-07-02 10:36:05 -0500

    day 10

---
+++ src/test/java/test/day10_jsexecutor_upload_actions/Uploading.java
@@ -0,0 +1,13 @@
+package test.day10_jsexecutor_upload_actions;
+
+import org.testng.annotations.Test;
+
+public class Uploading {
+
+    @Test
+    public void uploading_test(){
+        
+    }
+
+
+}
