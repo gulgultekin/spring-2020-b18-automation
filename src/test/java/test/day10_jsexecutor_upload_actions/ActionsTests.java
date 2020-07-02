d080ef3df2bb2276451bb016aa1bbe4e7dccd0de
gurhank <gurhankocyigit@gmail.com>
2020-07-02 11:30:56 -0500

    day 10

--- src/test/java/test/day10_jsexecutor_upload_actions/ActionsTests.java
+++ src/test/java/test/day10_jsexecutor_upload_actions/ActionsTests.java
@@ -1,12 +1,18 @@
 package test.day10_jsexecutor_upload_actions;
 
 import org.testng.annotations.Test;
+import utilities.Driver;
 
 public class ActionsTests {
 
     @Test
     public void hover_over_test1(){
-        
+        Driver.getDriver().get("https://www.amazon.com");
+
+        //1- Create the instance of Actions class
+        //2- Pass the current driver instance
+        //3- Locate the web element to hover over
+        //4- Using actions, hover over to the Language options
     }
 
 }
