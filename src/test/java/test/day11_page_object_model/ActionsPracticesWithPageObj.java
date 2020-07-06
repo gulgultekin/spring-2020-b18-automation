f6ede7ad4fdeed622945ef8e855ed6fd165edb38
gurhank <gurhankocyigit@gmail.com>
2020-07-06 15:32:20 -0500

    day11

--- src/test/java/test/day11_page_object_model/ActionsPracticesWithPageObj.java
+++ src/test/java/test/day11_page_object_model/ActionsPracticesWithPageObj.java
@@ -1,8 +1,16 @@
 package test.day11_page_object_model;
 
+import org.testng.annotations.Test;
+
 public class ActionsPracticesWithPageObj {
 
+    @Test
+    public void tc15_hover_over_test(){
 
+        
+
+
+    }
 
 
 }
