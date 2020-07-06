3a4b9e121985b92574e95fc02a4c6a474913375e
gurhank <gurhankocyigit@gmail.com>
2020-07-06 11:53:42 -0500

    day11

--- src/test/java/test/day11_page_object_model/NegativeLoginTests.java
+++ src/test/java/test/day11_page_object_model/NegativeLoginTests.java
@@ -1,12 +1,29 @@
 package test.day11_page_object_model;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
+import utilities.Driver;
 
 public class NegativeLoginTests {
 
     @Test
     public void negative_login_test(){
 
+        Driver.getDriver().get(ConfigurationReader.getProperty("vytrack_url"));
+
+        WebElement usernameInput = Driver.getDriver().findElement(By.id("prependedInput"));
+        WebElement passwordInput = Driver.getDriver().findElement(By.id("prependedInput2"));
+        WebElement loginButton = Driver.getDriver().findElement(By.id("_submit"));
+
+        String username = ConfigurationReader.getProperty("storemanager_username");
+
+        usernameInput.sendKeys(username);
+
+        passwordInput.sendKeys("lkjzdhvalk");
+
+        loginButton.click();
 
 
     }
