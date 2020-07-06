3395306af50ede7964d0d48a2d1bc9d3888f248a
gurhank <gurhankocyigit@gmail.com>
2020-07-06 10:27:25 -0500

    day 11

--- src/test/java/test/day11_page_object_model/ActionsPractices.java
+++ src/test/java/test/day11_page_object_model/ActionsPractices.java
@@ -3,10 +3,13 @@ package test.day11_page_object_model;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.Driver;
 
 public class ActionsPractices {
+
+    Actions actions;
 
     @Test
     public void scrolling_with_actions() throws InterruptedException{
@@ -14,7 +17,7 @@ public class ActionsPractices {
         Driver.getDriver().get("http://practice.cybertekschool.com/");
 
         //creating actions instance to be able to use its methods
-        Actions actions = new Actions(Driver.getDriver());
+        actions = new Actions(Driver.getDriver());
 
         //locating the web element we want to scroll to
         WebElement cybertekSchoolLink = Driver.getDriver().findElement(By.linkText("Cybertek School"));
@@ -28,4 +31,37 @@ public class ActionsPractices {
         actions.moveToElement(inputs).click().perform();
 
     }
+
+
+    @Test
+    public void tc16_double_click_test(){
+        //1. Go to https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2
+        Driver.getDriver().get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2");
+
+        //2. Switch to iframe.
+        // 1- index 2- id-name value 3-locate it as a webELement and pass it into
+        Driver.getDriver().switchTo().frame("iframeResult");
+
+        //3. Double click on the text “Double-click me to change my text color.”
+        WebElement textToDoubleClick = Driver.getDriver().findElement(By.id("demo"));
+
+        actions = new Actions(Driver.getDriver());
+
+        actions.doubleClick(textToDoubleClick).perform();
+
+
+        //4. Assert: Text’s “style” attribute value contains “red”.
+
+        String actual = textToDoubleClick.getAttribute("style");
+        String expected = "color: red;";
+
+        Assert.assertTrue(actual.equals(expected));
+
+
+
+
+
+    }
+
+
 }
