1933ba854ab1d310530edacee39b278aa4604426
gurhank <gurhankocyigit@gmail.com>
2020-07-06 13:18:30 -0500

    day11

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,8 +3,21 @@ package pages;
 We will store all Login page related web elements and methods into this class.
  */
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class LoginPage {
 
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "prependedInput")
+    public WebElement usernameInput;
+
     
+
 
 }
