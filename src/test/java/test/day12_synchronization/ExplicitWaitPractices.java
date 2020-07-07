8da293a0b3581a7114dbf711ff6a944a7e51a3de
gurhank <gurhankocyigit@gmail.com>
2020-07-07 13:55:40 -0500

    day 12

--- src/test/java/test/day12_synchronization/ExplicitWaitPractices.java
+++ src/test/java/test/day12_synchronization/ExplicitWaitPractices.java
@@ -1,7 +1,10 @@
 package test.day12_synchronization;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.Loading7Page;
 import utilities.Driver;
 
 public class ExplicitWaitPractices {
@@ -19,10 +22,20 @@ public class ExplicitWaitPractices {
         String url = "http://practice.cybertekschool.com/dynamic_loading/7";
         Driver.getDriver().get(url);
 
+        Loading7Page loading7Page = new Loading7Page();
+
+        //creating the instance of WebDriverWait to use its methods
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        String expectedTitleToBe = "Dynamic title";
+        //we are using the object to set up our EXPECTED CONDITION
+        wait.until(ExpectedConditions.titleIs(expectedTitleToBe));
 
+        //done message is displayed
+        Assert.assertTrue(loading7Page.doneMessage.isDisplayed());
 
-
+        //image is displayed
+        Assert.assertTrue(loading7Page.spongeBobImage.isDisplayed());
+        //BREAK UNTIL 3.05 PM EST
 
 
 
